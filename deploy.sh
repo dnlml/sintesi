9d564f83098d3ca3dce67eaa83635c2c20c15baa
dnlml <danielemeli86@gmail.com>
2025-05-21 11:26:15 +0200

    Remove deployment scripts and Docker configuration files to simplify project structure and deployment process.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use 22
-export PATH="$HOME/.local/share/pnpm:$PATH"
-cd ~/sites/sintesi/app
-pnpm install
-pm2 reload sintesi || pm2 start build/index.js --name sintesi
